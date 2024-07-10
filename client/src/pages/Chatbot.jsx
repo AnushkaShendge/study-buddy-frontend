1e1eda12295debadc8ca9ff948f42f84f093562c
AnushkaShendge <anushkashendge5@gmail.com>
2024-07-10 23:08:16 +0530

    .

--- client/src/pages/Chatbot.jsx
+++ client/src/pages/Chatbot.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { MdSend } from "react-icons/md";
 import { v4 as uuidv4 } from 'uuid';
 
