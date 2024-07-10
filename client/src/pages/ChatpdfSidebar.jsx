930ea0947b91631461a996485ed11d0915ded93f
AnushkaShendge <anushkashendge5@gmail.com>
2024-07-11 00:31:28 +0530

    ,,

--- client/src/pages/ChatpdfSidebar.jsx
+++ client/src/pages/ChatpdfSidebar.jsx
@@ -21,6 +21,7 @@ function ChatpdfSidebar({ isOpen, toggleSidebar }) {
         Array.from(pdfs).forEach((pdf, index) => {
             formData.append(`pdfs[${index}]`, pdf);
         });
+        console.log(formData)
 
         try {
             const response = await axios.post('http://localhost:8000/api/upload_pdfs/', formData, {
@@ -28,11 +29,15 @@ function ChatpdfSidebar({ isOpen, toggleSidebar }) {
                     'Content-Type': 'multipart/form-data'
                 }
             });
-            console.log(response.data.object);
             console.log('Upload successful', response.data);
         } catch (error) {
             console.error('Error uploading PDFs', error);
         }
+    };
+    const handleFileChange = (event) => {
+        const files = event.target.files;
+        setPdfs(files);
+        console.log(files); // Log the selected files
     };
     return (
         <aside className={`fixed h-screen ${theme === 'light' ? '' : 'bg-black text-white'}`}>
@@ -46,7 +51,7 @@ function ChatpdfSidebar({ isOpen, toggleSidebar }) {
                     <img src={robot} alt="Robot" className="mb-16 h-72" />
                     <div className="text-center ">
                         <div className="flex items-center justify-center"><h1 className="text-2xl flex gap-4 font-bold mb-4 leading-5"><FaFileDownload size={35} className="text-yellow-400" />PDF File's Section</h1></div>
-                        <input type="file" multiple onChange={(e) => setPdfs(e.target.files)} className="mb-2" />
+                        <input type="file" multiple onChange={handleFileChange} className="mb-2" />
                         <p className="text-md mb-4">Upload your PDF Files & Click on the Submit & Process Button</p>
                         <div className="flex items-center justify-center mt-8"><button className="bg-blue-500 text-white gap-2 flex py-2 px-4 rounded-lg hover:bg-blue-600" onClick={handleUpload}><LuUpload size={20}/>Upload PDFs</button></div>
                     </div>
